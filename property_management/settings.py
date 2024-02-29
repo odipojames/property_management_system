a2cb4c5c53f2b1fcabd352e8b91dfd301d805b7d
odipojames <odipojames12@gmail.com>
2024-02-29 13:27:55 +0300

    deploy to vercel

--- property_management/settings.py
+++ property_management/settings.py
@@ -164,6 +164,9 @@ STATICFILES_DIRS = [
 ]
 STATICFILES_DIRS = ( os.path.join('static'), )
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 
